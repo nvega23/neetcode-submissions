983d7673c8db9d4fa5dbef5119777186b195dd22
Nestor Vega <vega.nestor1@gmail.com>
2026-05-18 20:08:06 -0700

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        output = []
+
+        for char in s.lower():
+            if char.isalnum():
+                output.append(char)
+        return "".join(output) == "".join(output[::-1])
